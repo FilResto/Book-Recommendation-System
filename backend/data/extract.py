189e6ba0b4caf74b2c7b083b2575293dcbf7b6a4
Filippo Restori <resto84773@gmail.com>
2024-10-25 18:02:06 +0800

    df redy

--- backend/data/extract.py
+++ backend/data/extract.py
@@ -1,4 +1,12 @@
 import pandas as pd
-df = pd.read_csv('/data/booksAll.csv', usecols=['bookId','title','series','author','description','language','genres','pages','awards','bbevote','price'])
-prova = df.head()
-print(df)
+
+# Step 1: Carica il file CSV e seleziona solo le colonne desiderate
+df = pd.read_csv('booksAll.csv')
+
+# Filtra le colonne che ti interessano
+columns_to_keep = ['bookId', 'title', 'series', 'author', 'description', 'language', 'genres', 'pages', 'awards', 'bbeVotes', 'price']
+filtered_df = df[columns_to_keep]
+
+# Step 2: Verifica il contenuto del nuovo dataframe
+print(filtered_df.head())  # Stampa le prime righe del dataframe
+
