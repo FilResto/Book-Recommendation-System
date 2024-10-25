449d578e34d02b1276294bb2c4c70f1cadb15ef7
Luca Pellicciotti s331419 <s331419@studenti.polito.it>
2024-10-25 17:53:43 +0800

    estrazione_data

--- backend/data/extract.py
+++ backend/data/extract.py
@@ -1,2 +1,4 @@
 import pandas as pd
-df = pd.read_csv("books.csv")
+df = pd.read_csv('/data/booksAll.csv', usecols=['bookId','title','series','author','description','language','genres','pages','awards','bbevote','price'])
+prova = df.head()
+print(df)
