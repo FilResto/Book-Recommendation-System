06ac16b0f82e28cc3a2a0f73794e4cf6225f485c
Luca Pellicciotti s331419 <s331419@studenti.polito.it>
2024-10-27 14:54:03 +0800

    aggiunta lettura file

--- backend/CBF/cbf.py
+++ backend/CBF/cbf.py
@@ -8,3 +8,11 @@ print(df_book.head())
 with open("df_user.pkl","rb") as file:
     df_users = pickle.load(file)
 print(df_users.head())
+
+with open("df_ratings.pkl","rb") as file:
+    df_ratings = pickle.load(file)
+print(df_ratings.head())
+
+with open("df_visualization.pkl","rb") as file:
+    df_visualizations = pickle.load(file)
+print(df_visualizations.head())
