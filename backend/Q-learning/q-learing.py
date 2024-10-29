c14ddba5f590881fd1b5aeae7e9d26368ee4adf1
Luca Pellicciotti s331419 <s331419@studenti.polito.it>
2024-10-29 11:59:36 +0800

    q-learning start

---
+++ backend/Q-learning/q-learing.py
@@ -0,0 +1,13 @@
+import pickle
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+# 1. Funzione per caricare i dati
+def load_data():
+    with open('../data/df_book.pkl', 'rb') as file:
+        df_book = pickle.load(file)
+    with open("../data/df_visualization.pkl", "rb") as file:
+        df_visualizations = pickle.load(file)
+    with open("../data/df_ratings.pkl", "rb") as file:
+        df_ratings = pickle.load(file)
+    return df_book, df_ratings, df_visualizations
